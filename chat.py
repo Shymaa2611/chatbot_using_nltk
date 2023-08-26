adb0c7c5fa7156fd9b881cbc3376648df50370db
Your Name <you@example.com>
2023-08-26 20:55:56 +0200

    update chatbot

--- chat.py
+++ chat.py
@@ -1,18 +1,21 @@
 import nltk
 from nltk.chat.util import Chat
 
-pairs=[
-  [
-      "hello",
-      ["hi"]
-  ],
-    [
-      "How are You",
-      ["i ' am fine"]
-  ],
-
+data = [
+    ["hello", ["hi"]],
+    ["How are you", ["I'm fine"]],
+    ["what's the problem", ["I don't understand how to watch a video"]]
 ]
 
-print("How can help you ? ")
-chat=Chat(pairs)
-chat.converse()
+print("How can I help you ?")
+chat = Chat(data)
+
+while True:
+    data_input = input("You : ")
+    if data_input.lower() == "exit":
+        break
+    response = chat.respond(data_input)
+    if not response:
+        print("I'm sorry, I can'nt help you ")
+    else:
+        print(response)
